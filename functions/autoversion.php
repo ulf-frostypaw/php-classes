bd1db2075e22a465bc4a9f6b94a7003a1cf14ebf
Ulf FrostyPaw <79121980+ulf-frostypaw@users.noreply.github.com>
2021-12-11 21:35:47 -0600

    autoversion.php

--- functions/autoversion.php
+++ functions/autoversion.php
@@ -2,6 +2,11 @@
 /*
 * Genera automaticamente un control de versiones de archivos NO dinamicos. (Pueden ser js, css, etc...)
 * De /static/styles.css -> /static/styles.{version}.css
+* IMPORTANTE: 
+* AÑADIR EN UN ARCHIVO .htaccess
+* 
+    RewriteEngine On
+    RewriteRule ^(.*)\.[\d]{10}\.(css|js)$ $1.$2 [L]
 */
 function auto_version($file){
   if(strpos($file, '/') !== 0 || !file_exists($_SERVER['DOCUMENT_ROOT'] . $file))
